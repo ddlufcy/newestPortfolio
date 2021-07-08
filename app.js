1110d8ce468b2ac7b2106a27a581bc7647807b52
ddlufcy <ddlufcy@gmail.com>
2021-07-07 22:32:44 -0400

    transition text

--- app.js
+++ app.js
@@ -6,25 +6,40 @@ let boxSpan = document.querySelectorAll('.box');
 boxSpan.forEach((box) => {
 	box.addEventListener('click', () => {
 		box.classList.toggle('bigBox')	
-		console.log(boxWidths)
 	})
 })
 
 let topName = document.querySelector('#firstName');
 
 
-let initialWidth = null;
-
 let bigBox = document.querySelector('.bigBox')
 
 
-let about = document.getElementById('fullTextAbout')
+let skills = document.querySelector("#skills");
+
+skills.addEventListener('transitionend', () => {
+	if (skills.innerHTML === "Skills") {
+		console.log('text')
+		skills.innerHTML = "JavaScript SASS HTML5";
+	} else {
+		skills.innerHTML = "Skills";
+	}	
+})
+
+let about = document.querySelector('#about')
+about.addEventListener('transitionend', () => {
+	setTimeout(()=> {
+		let aboutMe = "About Me";
+	let aboutText = "Hi, I'm Daniel Lufcy. I'm a fullstack JavaScript developer in Indianapolis. I love to build things that people love to use. Feel free to reach out if you would be interested in using me for your next project.";
+	if (about.offsetWidth > 75) {
+		console.log(about.offsetWidth)
+		about.innerHTML = `${aboutText}`
+	} 
+	else {
+		console.log(about.offsetWidth)
+		about.innerHTML = "About Me"
+	}
+	}, 500)	
+})
 
 
-
-
-// grab initialWidth to compare
-window.addEventListener('load', (event) => {
-	initialWidth = window.innerWidth;;
-  });
-  
