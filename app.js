3ff7a382b7cf0c6a553dbce6d15f16596977beb0
ddlufcy <ddlufcy@gmail.com>
2021-07-09 19:49:13 -0400

    add ResizeObservers

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
 //logic for boxes
-let boxSpan = document.querySelectorAll('.box');
-
-
+let boxSpan = document.querySelectorAll(".box");
 
 boxSpan.forEach((box) => {
-	box.addEventListener('click', () => {
-		box.classList.toggle('bigBox')	
-	})
-})
+  box.addEventListener("click", () => {
+    box.classList.toggle("bigBox");
+  });
+});
 
-let topName = document.querySelector('#firstName');
+let topName = document.querySelector("#firstName");
 
+let bigBox = document.querySelector(".bigBox");
 
-let bigBox = document.querySelector('.bigBox')
-
-
-let skills = document.querySelector("#skills");
-
-skills.addEventListener('transitionend', () => {
-	if (skills.innerHTML === "Skills") {
-		console.log('text')
-		skills.innerHTML = "JavaScript SASS HTML5";
-	} else {
+//skills bubble
+const skills = document.querySelector("#skills");
+new ResizeObserver(() => {
+	if (skills.offsetWidth > 95) {
+		console.log("text");
+		skills.innerHTML = "Here are the languages and Libraries I use:" 
+		} else if (skills.offsetWidth < 95) {
 		skills.innerHTML = "Skills";
-	}	
-})
+		}
+}).observe(skills);
 
-let about = document.querySelector('#about')
-about.addEventListener('transitionend', () => {
-	setTimeout(()=> {
-		let aboutMe = "About Me";
-	let aboutText = "Hi, I'm Daniel Lufcy. I'm a fullstack JavaScript developer in Indianapolis. I love to build things that people love to use. Feel free to reach out if you would be interested in using me for your next project.";
-	if (about.offsetWidth > 75) {
-		console.log(about.offsetWidth)
-		about.innerHTML = `${aboutText}`
-	} 
-	else {
-		console.log(about.offsetWidth)
-		about.innerHTML = "About Me"
-	}
-	}, 500)	
-})
+//about bubble
+const about = document.querySelector("#about");
+new ResizeObserver(() => {
+  if (about.offsetWidth > 95) {
+	console.log("large");
+	about.textContent = "Hi, I'm Daniel Lufcy. I'm a fullstack JavaScript developer in Indianapolis. I love to build things that people love to use. Feel free to reach out if you would be interested in using me for your next project.";
+  } else if (about.offsetWidth < 100) {
+    console.log("small");
+    about.textContent = "About Me";
+  }
+}).observe(about);
 
 
